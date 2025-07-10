f58856c3c47eec731f53973a95f3c0b4d054d5c7
Yan Miguel Constantino Dos Santos <yanmiguel0gamil.com@gmail.com>
2025-07-09 22:52:28 -0300

    Add files via upload

--- scrpty.js
+++ scrpty.js
@@ -1,29 +1,23 @@
-const verResulatdo = document.getElementById('verresultado')
-verResulatdo.addEventListener('click' , res)
+const verResultado = document.getElementById('verResultado');
+verResultado.addEventListener('click' , res);
 
 function res(){
-    const nome = document.getElementById('nome').value
-    const notaUm = document.getElementById('notaum').value
-    const notaDois = document.getElementById('notadois').value
-    const resfinal = document.getElementById('resultado')
-    const nUm = parseInt(notaUm)
-    const nDois = parseInt(notaDois)
-    const somaNota = nUm + nDois
-    const media = somaNota / 2
-    
+    const nome = document.getElementById('nome').value;
+    const notaUm = document.getElementById('notaum').value;
+    const notaDois = document.getElementById('notadois').value;
+    const resfinal = document.getElementById('resultado');
+    const nUm = parseFloat(notaUm);
+    const nDois = parseFloat(notaDois);
+    const somaNota = (nUm + nDois) / 2;
     
 
     if (nome !== '' && notaUm !== '' && notaDois !== '' ){
-
-        
-
-        if(media >= 6){
-            resfinal.textContent = ' Parabéns '+ nome + ' Sua Nota Media Final foi ' + media + ' Voce Não Esta De Recuperação.'
-        }else if(media <= 6){
-            resfinal.textContent = nome + ' Infelizmente , Você Não Atingiu a Nossa Media. Sua Media Foi ' + media + ' Você Esta De Recuperacão.' 
+        if(somaNota >= 6){
+            resfinal.textContent = ' Parabéns '+ nome + ' Sua Nota Media Final foi ' + somaNota.toFixed(1) + ' Voce Não Esta De Recuperação.';
+        }else if(somaNota <= 6){
+            resfinal.textContent = nome + ' Infelizmente , Você Não Atingiu a Nossa Media. Sua Media Foi ' + somaNota.toFixed(1) + ' Você Esta De Recuperacão.';
         }
-
-    }else{
-        window.alert('Algum campo não foi prenchido')
+        }else{
+            window.alert('Algum campo não foi preenchido');
     }
 }
